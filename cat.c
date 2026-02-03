fdb233ef4091da95f71a2982edc3c343fb9b50ae
ivosoft-19 <53370330+ivosoft-19@users.noreply.github.com>
2026-02-03 20:36:06 +0000

    010

---
+++ cat.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    do
+    {
+      n = get_int("What's n? ");
+    } 
+    while (n < 0);
+    
+    for (int i = 0; i < n; i++)
+    {
+         printf("meow\n");
+    }
+}
